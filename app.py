91362c0c9ca55ed28a4c2ef13918c6890a9b8a98
this-is-mjk <mjlivetowork@gmail.com>
2024-05-22 13:06:20 +0530

    jwt token former

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from io import BytesIO
@@ -8,93 +8,138 @@ from deepface import DeepFace
 from PIL import Image
 import cv2
 from flask_cors import CORS
+from datetime import datetime, timedelta, timezone
+import jwt
+
+# Custom Exceptions for handling errors
+class missing_form_data(Exception):
+    # (Exception): This specifies that the new class MissingFormDataError inherits from the built-in Exception class. 
+    # By inheriting from Exception and pass is to avoid syntax error of empty class.
+    pass
+class face_not_detected(Exception):
+    pass
+class user_not_found(Exception):
+    pass
+class something_went_wrong(Exception):
+    pass
 
 #Load pretrained face detection model    
 net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
 
+# create a flask app with Cors to enable local development
 app = Flask(__name__)
 CORS(app)
-# CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
 
+app.config['SECRET_KEY'] = 'this_is_mjk_secret_key'
+
+# connect to database and open users in AttendenceApp database
 client = MongoClient('localhost', 27017)
 db = client.AttendenceApp
 users = db.users
 
-@app.route('/check-student', methods=['get'])
-def check_user():
-    data = request.get_json()
-    if not data or 'user_id' not in data or data['user_id'] == '':
-        return jsonify({'error': 'Bad Request'}), 400
-    #get user_id
-    user_id = data['user_id']
-    #check for user_id
-    if users.count_documents({'user_id': user_id}) > 0:
-        return jsonify({'exists': True}), 200
-    else:
-        return jsonify({'exists': False}), 200
+# Raising errors
+@app.errorhandler(missing_form_data)
+def handle_missing_form_data(error):
+    return jsonify({'error': "Bad Request, Missing Required Data"}), 400
+@app.errorhandler(face_not_detected)
+def handle_face_not_detected(error):
+    return jsonify({'error': "No face detected"}), 400
+@app.errorhandler(user_not_found)
+def handle_user_not_found(error):
+    return jsonify({'error': "User not found"}), 400
+@app.errorhandler(something_went_wrong)
+def handle_something_went_wrong(error):
+    return jsonify({'error': "Something went wrong, Please Try Again Later"}), 400
+
     
 @app.route('/add-student', methods=['post'])
 def add_student():
-    # get user_id and image form request
     try:
-        # print(request.form)
-        user_id = request.form['user_id']
-        image = request.files['image']
-        # image file already in binary data
+        # get user_id and image form request
+        user_id, image = extract_id_and_image(request)
+        # check if user already exist
+        if check_user_exist(user_id, users, raise_error=False):
+            return jsonify({'error': 'User already exist'}), 400
+        # Try save the image in database
         try: 
-            image_binary = extrat_face(image).read()
-            # print(image_binary)
+            users.insert_one({'user_id': user_id, 'image': extrat_face(image), 'attendence': []})
+            print('User added')
+            return jsonify({'status': 'student added'}), 200
+            # # to check what is added in database
+            # img1 = Image.open(extrat_face(image))
+            # img1.save('img.jpg')
         except Exception as e:
             print(e)
-            return jsonify({'error': 'No face detected'}), 400   
-        # save the image in database
-        users.insert_one({'user_id': user_id, 'image': image_binary})
-        print('User added')
-        # # to check what is added in database
-        # img1 = Image.open(extrat_face(image))
-        # img1.save('img.jpg')
-        return jsonify({'status': 'student added'}), 200
+            raise something_went_wrong('Something went wrong, Please Try Again Later')
     except Exception as e:
         print(e)
-        return jsonify({'error': 'Bad Request'}), 400
+        return jsonify({'error': str(e)}), 400
+    
 @app.route('/mark-attendence', methods=['post'])
 def mark_attendence():
     try:
-        # print(request.form)
-        user_id = request.form['user_id']
-        image = request.files['image']
-        # find the user in database
-        user = users.find_one({'user_id': user_id})
-        if user:
-            try: 
-                image_binary = extrat_face(image).read()
-                # print(image_binary)
-            except Exception as e:
-                print(e)
-                return jsonify({'error': 'No face detected'}), 400 
-            # compare image with the image in database
-            if check_face(BytesIO(image_binary), BytesIO(user['image'])):
+        # get user_id and image form request
+        user_id, image = extract_id_and_image(request)
+        #check if user exist
+        user = check_user_exist(user_id, users)
+        try:
+            # compare image with the image in database after extracting face
+            if check_face(BytesIO(extrat_face(image)), BytesIO(user['image'])):
+                # if face matched mark present
+                mark_present(user)
                 return jsonify({'status': 'attendence marked'}), 200
             else:
-                return jsonify({'status': 'attendence not marked, face did not matched'}), 200
-        else:
-            return jsonify({'status': 'student not found'}), 200
+                return jsonify({'status': 'attendence not marked, face did not matched'}), 400
+            
+        except Exception as e:
+            print(e)
+            return jsonify({'error': 'Something Bad Happened, Please Try Again'}), 400
     except Exception as e:
         print(e)
-        return jsonify({'error': 'Bad Request'}), 400
-    
+        return jsonify({'error': str(e)}), 400
 
+@app.route('/login', methods=['get'])
+def login():
+    # get user_id and image form request
+    user_id, image = extract_id_and_image(request)
+    #check if user exist
+    user = check_user_exist(user_id, users)
+    try:
+        # compare image with the image in database after extracting face
+        if check_face(BytesIO(extrat_face(image)), BytesIO(user['image'])):
+            # generates the JWT Token
+            token = jwt.encode({
+                'user_id': user_id,
+                'exp' : datetime.now(timezone.utc) + timedelta(minutes = 30)
+                # 30 min life of token
+            }, app.config['SECRET_KEY'])
+            # Set token in cookie
+            response = make_response(jsonify({'message': 'Login successful'}))
+            response.set_cookie('token', token)
+            return response
+        else: 
+            return jsonify({'error': 'Face did not matched'}), 400
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 400
+def extract_id_and_image(request):
+    try:
+        user_id = request.form['user_id']
+        image = request.files['image']
+        return user_id, image
+    except KeyError as e:
+        print(e)
+        raise missing_form_data('Missing form data') 
+    
 def check_face(image1, image2):
     # compare the image
     try: 
         # Convert BytesIO images to PIL Images
         img1 = Image.open(image1)
         img2 = Image.open(image2)
-        
         # Save PIL Images files
         img1.save('img1.jpg')
         img2.save('img2.jpg')
-
         # Use DeepFace to verify images
         result = DeepFace.verify('img1.jpg', 'img2.jpg', enforce_detection=False)
         # delete the memory used by the files
@@ -104,9 +149,10 @@ def check_face(image1, image2):
         return result['verified']
     except Exception as e:
         print(e)
-        return False
+        raise something_went_wrong('Something went wrong, Please Try Again Later')
     
 def extrat_face(image):
+    # image file already in binary data
     global net
     image = np.array(Image.open(image))
     (h,w) = image.shape[:2]
@@ -133,7 +179,28 @@ def extrat_face(image):
     image_binary = BytesIO()
     image.save(image_binary, 'JPEG')
     image_binary.seek(0)
+    try: 
+        image_binary = image_binary.read()
+        # print(image_binary)
+    except Exception as e:
+        print(e)
+        raise face_not_detected('No face detected')
     return image_binary
+
+def mark_present(user):
+    date = datetime.now().strftime('%Y-%m-%d %H:%M')
+    users.update_one(user, {'$push': {"attendence" : {date: 'present'}}})
+
+def check_user_exist(user_id, users, raise_error=True):
+    user = users.find_one({'user_id': user_id})
+    if user is not None:
+        return user
+    else:
+        if raise_error:
+            raise user_not_found('User not found')
+        else:
+            return False
+
 
 if __name__ == "__main__":
     app.run(host="localh.st", port=5000, debug=True)
