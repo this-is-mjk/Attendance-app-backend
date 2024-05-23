9bf3bafb97b48597bb847673e3d5d9fa333ace1e
this-is-mjk <mjlivetowork@gmail.com>
2024-05-23 20:26:09 +0530

    final readme

---
+++ README.md
@@ -0,0 +1,121 @@
+### Project Attendence App
+    -- By this_is_mjk Manas Jain Kuniya
+This app uses facial recogintion by DeepFace to mark attedence, mongodb as data base, python with flask for server, and react for frontend
+
+demo vedio link:- https://drive.google.com/file/d/1dHEqofvBiVSwp0DsM_fATmrwoBwefYhS/view?usp=sharing
+
+### github link 
+
+frontend:-https://github.com/this-is-mjk/Attendance-App.git
+
+backend:- https://github.com/this-is-mjk/Attendance-app-backend.git
+
+initially i thought of deploying them that's why i made 2 repos
+
+used vrsel to deploy the frontend, did't got time to deploy backend and data base, tries on GCP, did't worked, was thinking to look on AWS and Mongodb atlas.
+
+### Area of Improvement
+1. to make the code more readable by using file organising for different function
+2. JWT implementation intergration with the frontend.
+3. front end is very messy with a lot of code repetation, would like to organise the same and learn new concept to use multiple useStae variables across the react app without passing every where,
+4. React code can get full of errors in no time, i want to make it organised and compact so that it is esay to use at multiple places, modularity is my focus.
+
+### Features
+1.  User Authentication: JWT-based  authentication, workes with postman, issues with the react frontend
+2. Face Detection and verification
+3. Attendance Management
+4. Admin Power of Adding students, getting attendence and mark absent all
+
+### Used technologies
+1. Backend: Flask
+2. Database: MongoDB
+3. Facial Recognition: OpenCV, DeepFace
+4. Token Management: JWT
+
+
+### Backend
+
+### Pre requisites
+1. Python
+2. MongoDB compass
+3. pip for python
+
+### API Endpoints
+## Login
+    URL: /login
+    Method: POST
+    Description: Logs in the user and returns a JWT token.
+    Request:
+    Form Data:
+        user_id: User ID
+        image: User image file
+    Response:
+        Success: { "status": "Login successful", "isAdmin": boolean, "attendence": [] }
+        Error: { "status": "Error message" }
+## Mark Attendance
+    URL: /mark-attendance
+    Method: POST
+    Description: Marks the user's attendance.
+    Request:
+    Form Data:
+        user_id: User ID
+        image: User image file
+    Response:
+        Success: { "status": "attendance marked" }
+        Error: { "status": "Error message" }
+## Add Student
+    URL: /add-student
+    Method: POST
+    Description: Adds a new student to the system (Admin only).
+    Request:
+    Form Data:
+            user_id: User ID
+            image: User image file
+    Response:
+        Success: { "status": "student added" }
+        Error: { "status": "Error message" }
+## Get Attendance
+    URL: /get-attendance
+    Method: POST
+    Description: Gets the attendance records for the user or another user (Admin only).
+    Request:
+        Form Data:
+        user_id: User ID (of the user whose attendance is requested)
+    Response:
+        Success: { "status": "Got Attendance", "attendance": [] }
+        Error: { "status": "Error message" }
+## Mark Absent All
+    URL: /mark-absent-all
+    Method: GET
+    Description: Marks all users as absent who have not marked their attendance for the day (Admin only).
+    Response:
+        Success: { "status": "All other marked absent, count: n" }
+        Error: { "status": "Error message" }
+
+### Error Handling
+
+Custom exceptions are used to handle errors and return appropriate responses:
+1. missing_form_data
+2. face_not_detected
+3. user_not_found
+4. something_went_wrong
+5. not_admin
+
+### Helper Functions
+1. extract_id_and_image: Extracts user ID and image from the request.
+2. check_face: Compares two images to check if they have the same face.
+3. extrat_face: Extracts the face from an image.
+4. mark_present: Marks a user as present.
+5. check_and_get_use: Checks if a user exists and returns the user.
+6. check_admin: Checks if a user is an admin.
+7. mark_absent: Marks all users as absent who have not marked their attendance for the day.
+
+
+### what i learned for this project
+1. got my weekness of react, like cookies and a bit of await, known and will now work on it
+2. got to know i can ignore the CORS issue of localhost by using localh.st
+3. came to know about Flask and DeepFace technologies
+4. helped me to brush me up with a few mongo db resources and python
+5. came to know about pip freeze > requirements.txt to list all requirements easily.
+
+### fronend
--- app.py
+++ app.py
@@ -13,7 +13,9 @@ import jwt
 from functools import wraps
 
 
-# Custom Exceptions for handling errors
+
+
+# Custom Exceptions for handling errors and returning error easily
 class missing_form_data(Exception):
     # (Exception): This specifies that the new class MissingFormDataError inherits from the built-in Exception class. 
     # By inheriting from Exception and pass is to avoid syntax error of empty class.
@@ -27,13 +29,17 @@ class something_went_wrong(Exception):
 class not_admin(Exception):
     pass
 
-#Load pretrained face detection model    
+
+
+### Initalisation of connections ###
+#Load pretrained saved face detection model    
 net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
 
 # create a flask app with Cors to enable local development
 app = Flask(__name__)
 CORS(app)
 
+# creating the jwt secreat key, will save it in env when deploying
 app.config['SECRET_KEY'] = 'this_is_mjk_secret_key'
 
 # connect to database and open users in AttendenceApp database
@@ -41,7 +47,8 @@ client = MongoClient('localhost', 27017)
 db = client.AttendenceApp
 users = db.users
 
-# Raising errors
+
+# Raising handelling with requests
 @app.errorhandler(missing_form_data)
 def handle_missing_form_data(error):
     return jsonify({'status': "Bad Request, Missing Required Data"}), 400
@@ -58,7 +65,11 @@ def handle_something_went_wrong(error):
 def handle_not_admin(error):
     return jsonify({'status': "You are not admin"}), 401
 
-# Routes
+# my JWT token system workes good with postman but,
+# with react the cookies are not saved for some reason
+# so i have implemented the doe without JWT, you can verify it easily by just uncommenting a few lines
+
+# using the JWT this fucntion is the token checker
 def token_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -80,12 +91,17 @@ def token_required(f):
 
     return decorated_function
 
+
+### Routes ###
+
+# Login
+# creates a JWT token and return, also if not admin, it returns the attendence directly.
 @app.route('/login', methods=['post'])
 def login():
     print("working login")
-    # get user_id and image form request
+    # get user_id and image from request
     user_id, image = extract_id_and_image(request)
-    #check if user exist
+    # check if user exist
     user = check_and_get_use(user_id, users)
     try:
         # compare image with the image in database after extracting face
@@ -100,19 +116,25 @@ def login():
             print(token)
             admin = check_admin(user_id, users, "Login", raise_error=False)
             if admin:
+                # returns a simple response with admin value
                 response = make_response(jsonify({'status': 'Login successful', 'isAdmin': admin}))
                 response.set_cookie('token', token)
                 return response
             else: 
+                # returns the attendence with the response
                 response = make_response(jsonify({'status': 'Login successful','isAdmin': admin, 'attendence': user['attendence']}))
                 response.set_cookie('token', token)
                 return response
         else: 
             return jsonify({'status': 'Face did not matched'}), 401
     except Exception as e:
+        # handels all the exceptions with the extraction, checking_face 
+        # with the help of above defined exceptions
         print(e)
         return jsonify({'status': str(e)}), 400
 
+# Mark Attendence
+# Extracts image, find user, check face, then mark present.
 @app.route('/mark-attendence', methods=['post'])
 def mark_attendence():
     try:
@@ -136,12 +158,18 @@ def mark_attendence():
         print(e)
         return jsonify({'status': str(e)}), 400
 
+# Add Student
+# Path is allowed for any one Logged in but request will ne regeted if not admin
+# Allowed only for ADMIN to add student, which is id number 230626 from my database.
+# currently it is not usng the JWT @token_required, 
+# if you use postman it will work just uncomment the line
 @app.route('/add-student', methods=['post'])
-# @token_required
-# def add_student(admin_user_id):
-def add_student():
-    try:
-        admin_user_id = "230626"
+# @token_required                       #Uncomment
+# def add_student(admin_user_id):       #Uncomment
+#   try:                                #Uncomment
+def add_student():                      #Comment
+    try:                                #Comment
+        admin_user_id = "230626"        #Comment
         # check if admin
         check_admin(admin_user_id, users, 'add student')
         # get user_id and image form request
@@ -164,13 +192,17 @@ def add_student():
         print(e)
         return jsonify({'status': str(e)}), 400
 
+
+# Get Attendence
+# it it defined for any one who is logged in to access path
+# he/she can access his/her attendence, only admin is allowed to check other's attendence
 @app.route('/get-attendence', methods=['post'])
-# @token_required
-# def get_attendence(user_id):
-    # try:
-def get_attendence():
-    try:
-        user_id = "230626"
+# @token_required                       #Uncomment
+# def get_attendence(user_id):          #Uncomment
+    # try:                              #Uncomment
+def get_attendence():                   #Comment
+    try:                                #Comment
+        user_id = "230626"              #Comment
         required_attendence_user_id = extract_id_and_image(request, extract_image=False)
         if required_attendence_user_id != user_id:
             check_admin(user_id, users, 'get attendence')
@@ -183,13 +215,15 @@ def get_attendence():
         print(e)
         return jsonify({'status': str(e)}), 400
     
+# Mark Absent if not present
+# allowed only to admin, markes absent to who is not present on that day
 @app.route('/mark-absent-all', methods=['get'])
-# @token_required
-# def mark_absent_all_not_present_today(admin_user_id):
-#   try:
-def mark_absent_all_not_present_today():
-    try:
-        admin_user_id = "230626"
+# @token_required                                           #Uncomment
+# def mark_absent_all_not_present_today(admin_user_id):     #Uncomment
+#   try:                                                    #Uncomment
+def mark_absent_all_not_present_today():                    #Comment
+    try:                                                    #Comment
+        admin_user_id = "230626"                            #Comment    
         check_admin(admin_user_id, users, 'mark all other absent')
         return jsonify({'status': 'All other marked absent, count: ' + str(mark_absent(users))}), 200
     except Exception as e:
@@ -198,8 +232,11 @@ def mark_absent_all_not_present_today():
 
 
 
-# Helper functions
-def extract_id_and_image(request, extract_image=True):
+### Helper functions ###
+
+# Extract ID and Image from the request, 
+# Raise exception, Missing Data, which will be returned as response
+def extract_id_and_image(request, extract_image=True):  #if you dont have image to acess use false
     print(request)
     try:
         user_id = request.form['user_id']
@@ -208,37 +245,46 @@ def extract_id_and_image(request, extract_image=True):
         image = request.files['image']
         return user_id, image
     except KeyError as e:
-        print("1")
         print(e)
-        print("2")
         raise missing_form_data('Missing form data') 
-    
+
+# Sole of the program
+# Check_face it takes two two BytesIO image file to check if the images have same face
+# Raise Exception if need, like the model was not able to process file, it will rasie, somthing went wrong
 def check_face(image1, image2):
     # compare the image
     try: 
         # Convert BytesIO images to PIL Images
         img1 = Image.open(image1)
         img2 = Image.open(image2)
-        # Save PIL Images files
+        # Save PIL Images files as the deepface model takes path, 
+        # i have tried any  way to take PLI images itself but dint found any thing on the net
         img1.save('img1.jpg')
         img2.save('img2.jpg')
+
         # Use DeepFace to verify images
         # https://viso.ai/computer-vision/deepface/ refer to know about google facenet and other models
         # the results are more accurate when the images have similar lighting conditions, 
-        #prefer not to have shadow on your face
+        # prefer not to have shadow on your face
+
         result = DeepFace.verify('img1.jpg', 'img2.jpg', enforce_detection=False, model_name="Facenet")
         # delete the memory used by the files
         os.remove('img1.jpg')
         os.remove('img2.jpg')
+        # result is a dictionary of many filed, one of which is verified according to the model
         print(result)
         return result['verified'] 
     except Exception as e:
         print(e)
         raise something_went_wrong('Something went wrong, Please Try Again Later')
     
+# It is important to extract face as a image form the whole image, 
+# I have taken this fucntion form online and modfied it according to the inputs we have,
+# Extracting face better allow us to compare the images and less errors while processing the images by model.
 def extrat_face(image):
     # image file already in binary data
     global net
+    # convert it to numpy array
     image = np.array(Image.open(image))
     (h,w) = image.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(image, (300,300)), 1.0, 
@@ -248,6 +294,7 @@ def extrat_face(image):
     confidence = detections[0, 0, 0, 2]
 
     if confidence < 0.5:
+        # we raise error if face is not detected
         raise face_not_detected("No face detected")
     box = detections[0, 0, 0, 3:7] * np.array([w,h,w,h])
     (startX, startY, endX, endY) = box.astype('int')
@@ -258,13 +305,18 @@ def extrat_face(image):
         dim  = (int(w*r), 480)
         image = cv2.resize(image, dim)
     except Exception as e:
-        return None
+        # exception when the face is out of the image, like a portion is not in the frame.
+        # return None
+        raise something_went_wrong("Not Complete Face")
     
+    # We were working with a numpy array, now conver it back to image
     image = Image.fromarray(image)
+    # save the image to binary type, or Byte type which was the input
     image_binary = BytesIO()
     image.save(image_binary, 'JPEG')
     image_binary.seek(0)
     try: 
+        # Read it as a single line or array of chars and return
         image_binary = image_binary.read()
         # print(image_binary)
     except Exception as e:
@@ -272,12 +324,16 @@ def extrat_face(image):
         raise face_not_detected('No face detected')
     return image_binary
 
+# Mark Present Helper function.
 def mark_present(user):
+    # get date, time
     date = datetime.now().strftime('%Y-%m-%d')
     time = datetime.now().strftime('%H:%M')
+    # push object/dict with 'data' , 'status' , 'time' fields in the attendence array
     users.update_one(user, {'$push': {"attendence" : {'date': date, 'status': 'Present', 'time': time}}})
     print(user['user_id'] + " Present")
 
+# check User and return user, raise error if user not found
 def check_and_get_use(user_id, users, raise_error=True):
     user = users.find_one({'user_id': user_id})
     if user is not None:
@@ -288,6 +344,7 @@ def check_and_get_use(user_id, users, raise_error=True):
         else:
             return False
 
+# check admin, raise error if not, print the action taken by admin, like login, add student etc.
 def check_admin(admin_user_id, users, action, raise_error=True):
     try:
         admin = check_and_get_use(admin_user_id, users)
@@ -299,9 +356,12 @@ def check_admin(admin_user_id, users, action, raise_error=True):
         else:
             return False
 
+# Mark the students which do not have the present marked of today and return the count of such students
 def mark_absent(users):
+    # get time and date
     date = datetime.now().strftime('%Y-%m-%d')
     time = datetime.now().strftime('%H:%M')
+    # update the required student objects in data base
     result = users.update_many ({ "attendence": 
                                     { "$not": 
                                         {"$elemMatch": 
@@ -311,10 +371,11 @@ def mark_absent(users):
                                     } 
                                 }, 
                                 { "$push": { "attendence": { 'date': date, 'status': 'Absent', 'time': time} } })
+    # return the count of the students marked absent
     print("Modified Documents:", result.modified_count)
     return result.modified_count
 
-
+# star the server eith the python3 app.py
 if __name__ == "__main__":
     app.run(host="localh.st", port=5000, debug=True)
     # app.run()
